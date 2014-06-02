7946b72d66fc8f3da8ae469af0ebf44e5c9e4927
prataprc <prataprc@gmail.com>
2014-06-02 16:39:55 +0530

    formated copyright notice.

--- scanner_test.go
+++ scanner_test.go
@@ -1,67 +1,69 @@
 //  Copyright (c) 2013 Couchbase, Inc.
-//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
-//  except in compliance with the License. You may obtain a copy of the License at
-//    http://www.apache.org/licenses/LICENSE-2.0
-//  Unless required by applicable law or agreed to in writing, software distributed under the
-//  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-//  either express or implied. See the License for the specific language governing permissions
-//  and limitations under the License.
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+//  use this file except in compliance with the License. You may obtain a copy
+//  of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//  License for the specific language governing permissions and limitations
+//  under the License.
 
 package parsec
 
 import (
-	"reflect"
-	"testing"
+    "reflect"
+    "testing"
 )
 
 func TestClone(t *testing.T) {
-	text := []byte(`example text`)
-	s := NewScanner(text)
-	if !reflect.DeepEqual(s, s.Clone()) {
-		t.Fatal("Clone() method does not work as intended")
-	}
+    text := []byte(`example text`)
+    s := NewScanner(text)
+    if !reflect.DeepEqual(s, s.Clone()) {
+        t.Fatal("Clone() method does not work as intended")
+    }
 }
 
 func TestMatch(t *testing.T) {
-	text := []byte(`example text`)
-	ref := `exampl`
-	s := NewScanner(text)
-	m, s := s.Match(`^ex.*l`)
-	if string(m) != ref {
-		t.Fatalf("mismatch expected %s, got %s", ref, string(m))
-	}
-	expcur := 6
-	if s.GetCursor() != expcur {
-		t.Fatalf("expected cursor position %v, got %v", expcur, s.GetCursor())
-	}
+    text := []byte(`example text`)
+    ref := `exampl`
+    s := NewScanner(text)
+    m, s := s.Match(`^ex.*l`)
+    if string(m) != ref {
+        t.Fatalf("mismatch expected %s, got %s", ref, string(m))
+    }
+    expcur := 6
+    if s.GetCursor() != expcur {
+        t.Fatalf("expected cursor position %v, got %v", expcur, s.GetCursor())
+    }
 }
 
 func TestSkipWS(t *testing.T) {
-	text := []byte(`        `)
-	ref := `        `
-	s := NewScanner(text)
-	m, s := s.SkipWS()
-	if string(m) != ref {
-		t.Fatalf("mismatch expected %q, got %q", ref, string(m))
-	}
-	expcur := 8
-	if s.GetCursor() != expcur {
-		t.Fatalf("expected cursor position %v, got %v", expcur, s.GetCursor())
-	}
+    text := []byte(`        `)
+    ref := `        `
+    s := NewScanner(text)
+    m, s := s.SkipWS()
+    if string(m) != ref {
+        t.Fatalf("mismatch expected %q, got %q", ref, string(m))
+    }
+    expcur := 8
+    if s.GetCursor() != expcur {
+        t.Fatalf("expected cursor position %v, got %v", expcur, s.GetCursor())
+    }
 }
 
 func TestEndof(t *testing.T) {
-	text := []byte(`        `)
-	s := NewScanner(text)
-	_, s = s.SkipWS()
-	if s.Endof() == false {
-		t.Fatalf("expected end of text")
-	}
+    text := []byte(`        `)
+    s := NewScanner(text)
+    _, s = s.SkipWS()
+    if s.Endof() == false {
+        t.Fatalf("expected end of text")
+    }
 
-	text = []byte(`        text`)
-	s = NewScanner(text)
-	_, s = s.SkipWS()
-	if s.Endof() == true {
-		t.Fatalf("did not expect end of text")
-	}
+    text = []byte(`        text`)
+    s = NewScanner(text)
+    _, s = s.SkipWS()
+    if s.Endof() == true {
+        t.Fatalf("did not expect end of text")
+    }
 }
--- tools/parsec/parsec.go
+++ tools/parsec/parsec.go
@@ -1,56 +1,67 @@
+//  Copyright (c) 2013 Couchbase, Inc.
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+//  use this file except in compliance with the License. You may obtain a copy
+//  of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//  License for the specific language governing permissions and limitations
+//  under the License.
+
 package main
 
 import (
-	"flag"
-	"fmt"
-	"github.com/prataprc/goparsec"
-	eg "github.com/prataprc/goparsec/examples"
-	"io/ioutil"
-	"os"
+    "flag"
+    "fmt"
+    "github.com/prataprc/goparsec"
+    eg "github.com/prataprc/goparsec/examples"
+    "io/ioutil"
+    "os"
 )
 
 var options struct {
-	expr string
-	json string
+    expr string
+    json string
 }
 
 func arguments() {
-	flag.StringVar(&options.expr, "expr", "",
-		"Specify input file or arithmetic expression string")
-	flag.StringVar(&options.expr, "json", "",
-		"Specify input file or json string")
-	flag.Parse()
+    flag.StringVar(&options.expr, "expr", "",
+        "Specify input file or arithmetic expression string")
+    flag.StringVar(&options.expr, "json", "",
+        "Specify input file or json string")
+    flag.Parse()
 }
 
 func main() {
-	var n parsec.ParsecNode
+    var n parsec.ParsecNode
 
-	if options.expr != "" {
-		n = parseExpr(getText(options.expr))
-	} else if options.json != "" {
-		n = parseExpr(getText(options.expr))
-	}
-	fmt.Println(n)
+    if options.expr != "" {
+        n = parseExpr(getText(options.expr))
+    } else if options.json != "" {
+        n = parseExpr(getText(options.expr))
+    }
+    fmt.Println(n)
 }
 
 func parseExpr(text string) parsec.ParsecNode {
-	s := parsec.NewScanner([]byte(text))
-	n, _ := eg.Expr(s)
-	return n
+    s := parsec.NewScanner([]byte(text))
+    n, _ := eg.Expr(s)
+    return n
 }
 
 func parseJSON(text string) parsec.ParsecNode {
-	n := eg.JSONParse([]byte(text))
-	return n
+    n := eg.JSONParse([]byte(text))
+    return n
 }
 
 func getText(filename string) string {
-	if _, err := os.Stat(filename); err != nil {
-		return filename
-	}
-	if b, err := ioutil.ReadFile(filename); err != nil {
-		panic(err)
-	} else {
-		return string(b)
-	}
+    if _, err := os.Stat(filename); err != nil {
+        return filename
+    }
+    if b, err := ioutil.ReadFile(filename); err != nil {
+        panic(err)
+    } else {
+        return string(b)
+    }
 }
