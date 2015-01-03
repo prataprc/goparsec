45dfd1d553940465dc1df4a0538168a2b5fbbcbd
prataprc <prataprc@gmail.com>
2015-01-03 23:33:14 +0530

    A decent implementation of parser combinator.
    After couple of trial and errors, following idea is used
    to implemented a parser combinator in a static language
    like golang.
    a. forward declaration.
    b. pointer reference to mutually referencing parsers.
    
    Normally it is easy to construct Terminal parsers and simple
    NonTerminal parsers.
    
    To construct NonTerminal parsers, whose decendants backreference
    to itself or to its ancestors, we can forward
    declare such non-terminals and pass them as pointer reference
    while combining them with other parsers. Subsequently we can
    initialize them under init() function.
    
    EG combinators for parsing JSON:
    
    // Forward declaration of single JSON value
    var value parsec.Parser
    
    // Combine them with reference
    // values -> value | values "," value
    var values = parsec.Kleene(valuesNode, &value, comma())
    // array -> "[" values "]"
    var array = parsec.And(arrayNode, openSqrt(), values, closeSqrt())
    ....
    
    func init() {
        // deferred initialization of parsers.
        value = parsec.OrdChoice(
           valueNode, parsec.Parser(tokenTerm), &array, &object)
        ....
    }

--- parsec.go
+++ parsec.go
@@ -1,101 +1,99 @@
-//  Copyright (c) 2013 Couchbase, Inc.
-//  Licensed under the Apache License, Version 2.0 (the "License"); you may not
-//  use this file except in compliance with the License. You may obtain a copy
-//  of the License at
-//      http://www.apache.org/licenses/LICENSE-2.0
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-//  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-//  License for the specific language governing permissions and limitations
-//  under the License.
+// Copyright (c) 2013 Couchbase, Inc.
 
-// Package parsec implements a library of parser-combinators using basic
-// recognizers like - And, OrdChoice, Kleene, Many and Maybe.
+// Package parsec implements a library of parser-combinators
+// using basic recognizers like,
 //
-// parsec tool's combinators can be used to construct higher-order parsers
-// using basic parser function. All parser functions are expected to follow
-// the `Parser` type signature, accepting a `Scanner` interface and returning
-// a `ParsecNode` and a new scanner. If a parser fails to match the input string
-// according to its rules, then it must return nil for ParsecNode, and a new
+//      And OrdChoice Kleene Many Maybe
+//
+// Parser combinators can be used to construct higher-order
+// parsers using basic parser function. All parser functions are
+// expected to follow the `Parser` type signature, accepting a
+// `Scanner` interface and returning a `ParsecNode` and a new
+// scanner. If a parser fails to match the input string according
+// to its rules, then it must return nil for ParsecNode, and a new
 // Scanner.
 //
-// ParsecNode can either be a Terminal structure or NonTerminal structure or a
-// list of Terminal/NonTerminal structure. The AST output is expected to be
-// made up of ParsecNode.
+// ParsecNode can either be a Terminal structure or NonTerminal
+// structure or a list of Terminal/NonTerminal structure. The AST
+// output is expected to be made up of ParsecNode.
 //
-// Nodify is a callback function that every combinators use as a callback
-// to construct a ParsecNode.
+// Nodify is a callback function that every combinators use as a
+// callback to construct a ParsecNode.
 package parsec
 
-import (
-	"fmt"
-)
+import "fmt"
 
 // ParsecNode type defines a node in the AST
 type ParsecNode interface{}
 
-// Parser function parses input text, higher order parsers are constructed
-// using combinators.
+// Parser function parses input text, higher order parsers are
+// constructed using combinators.
 type Parser func(Scanner) (ParsecNode, Scanner)
 
-// Nodify are callback function to construct ParsecNode
+// Nodify callback function to construct custom ParsecNode.
 type Nodify func([]ParsecNode) ParsecNode
 
-// Terminal structure can be used to construct a terminal ParsecNode.
+// Terminal structure can be used to construct a terminal
+// ParsecNode.
 type Terminal struct {
 	Name     string // contains terminal's token type
 	Value    string // value of the terminal
 	Position int    // Offset into the text stream where token was identified
 }
 
-// NonTerminal structure can be used to construct a non-terminal ParsecNode.
+// NonTerminal structure can be used to construct a
+// non-terminal ParsecNode.
 type NonTerminal struct {
 	Name     string       // contains terminal's token type
 	Value    string       // value of the terminal
 	Children []ParsecNode // list of children to this node.
 }
 
-// Scanner interface supplies necessary methods to match the input stream.
+// Scanner interface supplies necessary methods to match the
+// input stream.
 type Scanner interface {
-	// Clone will return new clone of the underlying scanner structure. This
-	// will be used by combinators to _backtrack_.
+	// Clone will return new clone of the underlying scanner structure.
+	// This will be used by combinators to _backtrack_.
 	Clone() Scanner
 
 	// GetCursor gets the current cursor position inside input text.
 	GetCursor() int
 
-	// Match the input stream with `pattern` and return matching string after
-	// advancing the cursor.
+	// Match the input stream with `pattern` and return
+	// matching string after advancing the cursor.
 	Match(pattern string) ([]byte, Scanner)
 
-	// OddMatch the input stream with a choice of `patterns` and return matching
-	// string and submatches, after advancing the cursor.
+	// SubmatchAll the input stream with a choice of `patterns`
+	// and return matching string and submatches, after
+	// advancing the cursor.
 	SubmatchAll(pattern string) ([][]byte, Scanner)
 
-	// SkipWs skips white space characters in the input stream. Return skipped
-	// whitespaces as byte-slice and advance the cursor.
+	// SkipWs skips white space characters in the input stream.
+	// Return skipped whitespaces as byte-slice and advance the cursor.
 	SkipWS() ([]byte, Scanner)
 
-	// Endof detects whether end-of-file is reached in the input stream and
-	// return a boolean indicating the same.
+	// Endof detects whether end-of-file is reached in the input
+	// stream and return a boolean indicating the same.
 	Endof() bool
 }
 
-// And combinator accepts a list of `Parser` that must match the input string,
-// atleast until the last Parser argument, and returns a parser function that
+// And combinator accepts a list of `Parser`, or reference to a
+// parser, that must match the input string, atleast until the
+// last Parser argument. Returns a parser function that
 // can be used to construct higher-level parsers.
 //
-// If all parser matches a list of ParsecNode, where each ParsecNode is
-// constructed by matching parser, will be passed as argument to Nodify
-// callback. Even if one of the input parser function fails then empty slice
-// of ParsecNode will be supplied as argument to Nodify callback.
-func And(callb Nodify, parsers ...Parser) Parser {
+// If all parser matches, a list of ParsecNode, where each
+// ParsecNode is constructed by matching parser, will be passed
+// as argument to Nodify callback. Even if one of the input
+// parser function fails then empty slice of ParsecNode will
+// be supplied as argument to Nodify callback.
+func And(callb Nodify, parsers ...interface{}) Parser {
 	return func(s Scanner) (ParsecNode, Scanner) {
 		var ns = make([]ParsecNode, 0, len(parsers))
 		var n ParsecNode
 		news := s.Clone()
 		for _, parser := range parsers {
-			n, news = parser(news)
+			n, news = doParse(parser, news)
 			if n == nil {
 				return nil, s
 			}
@@ -105,18 +103,18 @@ func And(callb Nodify, parsers ...Parser) Parser {
 	}
 }
 
-// OrdChoice combinator accepts a list of `Parser`, where atleast one of the
-// parser must match the input string, and returns a parser function that can be
-// used to construct higher level parsers.
+// OrdChoice combinator accepts a list of `Parser`, or
+// reference to a parser, where atleast one of the parser
+// must match the input string. Returns a parser function
+// that can be used to construct higher level parsers.
 //
-// The first matching parser function's output passed as argument to Nodify
-// callback. If non of the parsers match the input, then `nil` is returned for
-// ParsecNode
-func OrdChoice(callb Nodify, parsers ...Parser) Parser {
+// The first matching parser function's output is passed
+// as argument to Nodify callback. If non of the parsers
+// match the input, then `nil` is returned for ParsecNode
+func OrdChoice(callb Nodify, parsers ...interface{}) Parser {
 	return func(s Scanner) (ParsecNode, Scanner) {
 		for _, parser := range parsers {
-			n, news := parser(s.Clone())
-			if n != nil {
+			if n, news := doParse(parser, s.Clone()); n != nil {
 				return docallback(callb, []ParsecNode{n}), news
 			}
 		}
@@ -124,26 +122,31 @@ func OrdChoice(callb Nodify, parsers ...Parser) Parser {
 	}
 }
 
-// Kleene combinator accepts two parsers, namely opScan and sepScan, where
-// opScan parser will be used to match input string and contruct ParsecNode
-// and sepScan parser will be used to match input string and ignore the
-// matched string. If sepScan parser is not supplied, then opScan parser will
-// be applied on the input until it fails.
+// Kleene combinator accepts two parsers, or reference to
+// parsers, namely opScan and sepScan, where opScan parser
+// will be used to match input string and contruct ParsecNode
+// and sepScan parser will be used to match input string
+// and ignore the matched string. If sepScan parser is not
+// supplied, then opScan parser will be applied on the input
+// until it fails.
 //
-// The process of matching opScan parser and sepScan parser will continue in a
-// loop until either one of them fails on the input stream.
+// The process of matching opScan parser and sepScan parser
+// will continue in a loop until either one of them fails on
+// the input stream.
 //
-// For every successful match of opScan, the returned ParsecNode from matching
-// parser will be accumulated and passed as argument to Nodify callback.
-// If there is not a single match for opScan, then an empty slice of
-// ParsecNode will be passed as argument to Nodify callback.
-func Kleene(callb Nodify, parsers ...Parser) Parser {
-	var opScan, sepScan Parser
-	opScan = parsers[0]
-	l := len(parsers)
-	if l == 2 {
-		sepScan = parsers[1]
-	} else if l > 2 {
+// For every successful match of opScan, the returned
+// ParsecNode from matching parser will be accumulated and
+// passed as argument to Nodify callback. If there is not a
+// single match for opScan, then an empty slice of ParsecNode
+// will be passed as argument to Nodify callback.
+func Kleene(callb Nodify, parsers ...interface{}) Parser {
+	var opScan, sepScan interface{}
+	switch l := len(parsers); l {
+	case 1:
+		opScan = parsers[0]
+	case 2:
+		opScan, sepScan = parsers[0], parsers[1]
+	default:
 		panic(fmt.Errorf("Kleene parser doesn't accept %v parsers", l))
 	}
 	return func(s Scanner) (ParsecNode, Scanner) {
@@ -151,13 +154,12 @@ func Kleene(callb Nodify, parsers ...Parser) Parser {
 		ns := make([]ParsecNode, 0)
 		news := s.Clone()
 		for {
-			n, news = opScan(news)
-			if n == nil {
+			if n, news = doParse(opScan, news); n == nil {
 				break
 			}
 			ns = append(ns, n)
 			if sepScan != nil {
-				if n, news = sepScan(news); n == nil {
+				if n, news = doParse(sepScan, news); n == nil {
 					break
 				}
 			}
@@ -166,43 +168,49 @@ func Kleene(callb Nodify, parsers ...Parser) Parser {
 	}
 }
 
-// Many combinator accepts two parsers, namely opScan and sepScan, where
-// opScan parser will be used to match input string and contruct ParsecNode
-// and sepScan parser will be used to match input string and ignore the
-// matched string. If sepScan parser is not supplied, then opScan parser will
-// be applied on the input until it fails.
+// Many combinator accepts two parsers, or reference to
+// parsers, namely opScan and sepScan, where opScan parser
+// will be used to match input string and contruct ParsecNode
+// and sepScan parser will be used to match input string and
+// ignore the matched string. If sepScan parser is not
+// supplied, then opScan parser will be applied on the input
+// until it fails.
 //
-// The process of matching opScan parser and sepScan parser will continue in a
-// loop until either one of them fails on the input stream.
+// The process of matching opScan parser and sepScan parser
+// will continue in a loop until either one of them fails on
+// the input stream.
 //
-// The difference between `Many` combinator and `Kleene` combinator is that
-// there should atleast be one match of opScan.
+// The difference between `Many` combinator and `Kleene`
+// combinator is that there shall atleast be one match of opScan.
 //
-// For every successful match of opScan, the returned ParsecNode from matching
-// parser will be accumulated and passed as argument to Nodify callback. If
-// there is not a single match for opScan, then `nil` will be returned for
+// For every successful match of opScan, the returned
+// ParsecNode from matching parser will be accumulated and
+// passed as argument to Nodify callback. If there is not a
+// single match for opScan, then `nil` will be returned for
 // ParsecNode.
-func Many(callb Nodify, parsers ...Parser) Parser {
-	var opScan, sepScan Parser
-	opScan = parsers[0]
-	if len(parsers) >= 2 {
-		sepScan = parsers[1]
+func Many(callb Nodify, parsers ...interface{}) Parser {
+	var opScan, sepScan interface{}
+	switch l := len(parsers); l {
+	case 1:
+		opScan = parsers[0]
+	case 2:
+		opScan, sepScan = parsers[0], parsers[1]
+	default:
+		panic(fmt.Errorf("Many parser doesn't accept %v parsers", l))
 	}
 	return func(s Scanner) (ParsecNode, Scanner) {
 		var n ParsecNode
 		ns := make([]ParsecNode, 0)
 		news := s.Clone()
 		for {
-			n, news = opScan(news)
-			if n != nil {
-				ns = append(ns, n)
-				if sepScan != nil {
-					if n, news = sepScan(news); n == nil {
-						break
-					}
-				}
-			} else {
+			if n, news = doParse(opScan, news); n == nil {
 				break
+			}
+			ns = append(ns, n)
+			if sepScan != nil {
+				if n, news = doParse(sepScan, news); n == nil {
+					break
+				}
 			}
 		}
 		if len(ns) == 0 {
@@ -212,19 +220,33 @@ func Many(callb Nodify, parsers ...Parser) Parser {
 	}
 }
 
-// Maybe combinator accepts a single parser, and tries to match the input
-// stream with it.
-func Maybe(callb Nodify, parser Parser) Parser {
+// Maybe combinator accepts a single parser, or reference to
+// a parser, and tries to match the input stream with it.
+func Maybe(callb Nodify, parser interface{}) Parser {
 	return func(s Scanner) (ParsecNode, Scanner) {
-		n, news := parser(s.Clone())
+		n, news := doParse(parser, s.Clone())
 		if n == nil {
-			return []ParsecNode{}, s
+			return nil, s
 		}
 		return docallback(callb, []ParsecNode{n}), news
 	}
 }
 
-//---- Local function
+//---------------
+// Local function
+//---------------
+
+func doParse(parser interface{}, s Scanner) (ParsecNode, Scanner) {
+	switch p := parser.(type) {
+	case Parser:
+		return p(s)
+	case *Parser:
+		return (*p)(s)
+	default:
+		panic(fmt.Errorf("type of parser %T not supported", parser))
+	}
+	return nil, nil
+}
 
 func docallback(callb Nodify, ns []ParsecNode) ParsecNode {
 	if callb != nil {
--- scanner.go
+++ scanner.go
@@ -1,31 +1,19 @@
-//  Copyright (c) 2013 Couchbase, Inc.
-//  Licensed under the Apache License, Version 2.0 (the "License"); you may not
-//  use this file except in compliance with the License. You may obtain a copy
-//  of the License at
-//      http://www.apache.org/licenses/LICENSE-2.0
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-//  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-//  License for the specific language governing permissions and limitations
-//  under the License.
-
-// scanner to parse terminals from input text.
+// Copyright (c) 2013 Couchbase, Inc.
 
 package parsec
 
-import (
-	"regexp"
-)
+import "regexp"
 
-// SimpleScanner implements Scanner interface based on golang's regexp module.
+// SimpleScanner implements Scanner interface based on
+// golang's regexp module.
 type SimpleScanner struct {
 	buf      []byte                    // input buffer
 	cursor   int                       // cursor within input buffer
 	patterns map[string]*regexp.Regexp // cache of compiled regular expression
 }
 
-// NewScanner creates and returns a reference to new instance of SimpleScanner
-// object.
+// NewScanner creates and returns a reference to new instance
+// of SimpleScanner object.
 func NewScanner(text []byte) Scanner {
 	return &SimpleScanner{
 		buf:      text,
@@ -34,7 +22,7 @@ func NewScanner(text []byte) Scanner {
 	}
 }
 
-// Clone method receiver in Scanner interface.
+// Clone method receiver in Scanner{} interface.
 func (s *SimpleScanner) Clone() Scanner {
 	return &SimpleScanner{
 		buf:      s.buf,
@@ -43,27 +31,21 @@ func (s *SimpleScanner) Clone() Scanner {
 	}
 }
 
-// GetCursor method receiver in Scanner interface.
+// GetCursor method receiver in Scanner{} interface.
 func (s *SimpleScanner) GetCursor() int {
 	return s.cursor
 }
 
-// Match method receiver in Scanner interface.
+// Match method receiver in Scanner{} interface.
 func (s *SimpleScanner) Match(pattern string) ([]byte, Scanner) {
-	var regc *regexp.Regexp
 	var err error
-
-	if pattern[0] != '^' {
-		panic("match patterns must begin with `^`")
-	}
-
-	regc = s.patterns[pattern]
+	regc := s.patterns[pattern]
 	if regc == nil {
-		if regc, err = regexp.Compile(pattern); err == nil {
-			s.patterns[pattern] = regc
-		} else {
-			panic(err.Error())
+		regc, err = regexp.Compile(pattern)
+		if err != nil {
+			panic(err)
 		}
+		s.patterns[pattern] = regc
 	}
 	if token := regc.Find(s.buf[s.cursor:]); token != nil {
 		s.cursor += len(token)
@@ -72,33 +54,31 @@ func (s *SimpleScanner) Match(pattern string) ([]byte, Scanner) {
 	return nil, s
 }
 
-// SubmatchAll method receiver in Scanner interface.
+// SubmatchAll method receiver in Scanner{} interface.
 func (s *SimpleScanner) SubmatchAll(pattern string) ([][]byte, Scanner) {
-	var regc *regexp.Regexp
 	var err error
-
-	regc = s.patterns[pattern]
+	regc := s.patterns[pattern]
 	if regc == nil {
-		if regc, err = regexp.Compile(pattern); err == nil {
-			s.patterns[pattern] = regc
-		} else {
+		regc, err = regexp.Compile(pattern)
+		if err != nil {
 			panic(err.Error())
 		}
+		s.patterns[pattern] = regc
 	}
-	toks := regc.FindAllSubmatch(s.buf[s.cursor:], 1)
-	if len(toks) == 1 && toks[0] != nil && len(toks[0]) > 0 {
-		s.cursor += len(toks[0][0])
-		return toks[0], s
+	toks := regc.FindSubmatch(s.buf[s.cursor:])
+	if toks != nil {
+		s.cursor += len(toks[0])
+		return toks, s
 	}
 	return nil, s
 }
 
-// SkipWS method receiver in Scanner interface.
+// SkipWS method receiver in Scanner{} interface.
 func (s *SimpleScanner) SkipWS() ([]byte, Scanner) {
 	return s.Match(`^[ \t\r\n]+`)
 }
 
-// Endof method receiver in Scanner interface.
+// Endof method receiver in Scanner{} interface.
 func (s *SimpleScanner) Endof() bool {
 	if s.cursor >= len(s.buf) {
 		return true
