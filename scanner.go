4dd0ff08f2e3c49f9697a8868cf9e5da14e04f10
prataprc <prataprc@gmail.com>
2017-06-12 11:20:15 +0530

    SimpleScanner: Track lineno, close #20.

--- scanner.go
+++ scanner.go
@@ -4,6 +4,7 @@ package parsec
 
 import "regexp"
 import "bytes"
+import "strings"
 
 // Scanner interface supplies necessary methods to match the
 // input stream.
@@ -49,8 +50,11 @@ type Scanner interface {
 type SimpleScanner struct {
 	buf          []byte // input buffer
 	cursor       int    // cursor within input buffer
+	lineno       int
 	patternCache map[string]*regexp.Regexp
 	wsPattern    string // white space pattern used by SkipWS()
+	// settings
+	tracklineno bool
 }
 
 // NewScanner creates and returns a reference to new instance
@@ -59,8 +63,10 @@ func NewScanner(text []byte) Scanner {
 	return &SimpleScanner{
 		buf:          text,
 		cursor:       0,
+		lineno:       1,
 		patternCache: make(map[string]*regexp.Regexp),
 		wsPattern:    `^[ \t\r\n]+`,
+		tracklineno:  false,
 	}
 }
 
@@ -69,9 +75,15 @@ func (s *SimpleScanner) Clone() Scanner {
 	return &SimpleScanner{
 		buf:          s.buf,
 		cursor:       s.cursor,
+		lineno:       s.lineno,
 		patternCache: s.patternCache,
 		wsPattern:    s.wsPattern,
 	}
+}
+
+func (s *SimpleScanner) TrackLineno() *SimpleScanner {
+	s.tracklineno = true
+	return s
 }
 
 // GetCursor method receiver in Scanner{} interface.
@@ -83,6 +95,9 @@ func (s *SimpleScanner) GetCursor() int {
 func (s *SimpleScanner) Match(pattern string) ([]byte, Scanner) {
 	regc := s.getPattern(pattern)
 	if token := regc.Find(s.buf[s.cursor:]); token != nil {
+		if s.tracklineno && len(token) > 0 {
+			s.lineno += len(bytes.Split(token, []byte{'\n'})) - 1
+		}
 		s.cursor += len(token)
 		return token, s
 	}
@@ -96,6 +111,9 @@ func (s *SimpleScanner) MatchString(str string) (bool, Scanner) {
 		return false, s
 	} else if bytes.Compare(s.buf[s.cursor:s.cursor+ln], []byte(str)) != 0 {
 		return false, s
+	}
+	if s.tracklineno && len(str) > 0 {
+		s.lineno += len(strings.Split(str, "\n")) - 1
 	}
 	s.cursor += ln
 	return true, s
@@ -116,6 +134,9 @@ func (s *SimpleScanner) SubmatchAll(
 				continue
 			}
 			captures[name] = matches[i]
+		}
+		if s.tracklineno && len(matches[0]) > 0 {
+			s.lineno += len(bytes.Split(matches[0], []byte{'\n'})) - 1
 		}
 		s.cursor += len(matches[0])
 		return captures, s
